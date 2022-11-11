b786e43783a186afd214213e6c1802c111d190e5
Obriel Zvoushe <obrielzvoushe1@gmail.com>
2022-11-11 22:46:51 +0200

    Created form and input fields

--- fullstack-front/src/users/AddUser.js
+++ fullstack-front/src/users/AddUser.js
@@ -5,10 +5,53 @@ export default function AddUser() {
         <div className="container">
             <div className="row">
                 <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-                <h2 className="text-center m-4">Register User</h2>
-                
+                    <h2 className="text-center m-4">Register User</h2>
+                    <form>
+                    <div className="mb-3">
+                        <label htmlFor="Name" className="form-label">
+                            Name
+                        </label>
+                        <input
+                            type={"text"}
+                            className="form-control"
+                            placeholder="Enter your name"
+                            name="name"
+
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="Username" className="form-label">
+                            Username
+                        </label>
+                        <input
+                            type={"text"}
+                            className="form-control"
+                            placeholder="Enter your username"
+                            name="username"
+
+                        />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="Email" className="form-label">
+                            E-mail
+                        </label>
+                        <input
+                            type={"text"}
+                            className="form-control"
+                            placeholder="Enter your e-mail address"
+                            name="email"
+
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-outline-primary">
+                        Submit
+                    </button>
+                    <button className="btn btn-outline-danger mx-2">
+                        Cancel
+                    </button>
+                    </form>
                 </div>
-            </div> 
+            </div>
         </div>
     )
 }
