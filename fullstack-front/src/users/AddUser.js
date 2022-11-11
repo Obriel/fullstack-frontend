461a7da095c856f1d9b606dc12520f74cda90023
Obriel Zvoushe <obrielzvoushe1@gmail.com>
2022-11-11 22:21:09 +0200

    Created a div for the register user form

--- fullstack-front/src/users/AddUser.js
+++ fullstack-front/src/users/AddUser.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default function AddUser() {
     return (
-        <div>
-            AddUser Form
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
+                <h2 className="text-center m-4">Register User</h2>
+                
+                </div>
+            </div> 
         </div>
     )
 }
